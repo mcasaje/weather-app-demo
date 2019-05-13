f79bb9e9619e848591b672dc399e0ac5ced3cca4
Mark Casaje <mcasaje@gmail.com>
2019-05-12 21:27:19 -0700

    [1] request.ts: Impl Request.get() calls requestPromise.get()

--- src/libs/request.ts
+++ src/libs/request.ts
@@ -1,5 +1,9 @@
+import * as requestPromise from 'request-promise'
+
 namespace Request {
-  export const get = (url: string) => {}
+  export const get = (url: string) => {
+    requestPromise.get(url)
+  }
 }
 
 export default Request
