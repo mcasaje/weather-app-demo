0d1ee62302d2d0afab18638467313992b90c4983
Mark Casaje <mcasaje@gmail.com>
2019-05-12 22:14:11 -0700

    [1] html-parser.spec.ts: Unit test to check findElement() returns element based on selector

--- test/libs/html-parser.spec.ts
+++ test/libs/html-parser.spec.ts
@@ -7,10 +7,17 @@ describe('HtmlParser', () => {
     it('is defined', () => {
       expect(HtmlParser.findElement).toBeDefined()
     })
-    it("calls 'cheerio.load()'", () => {
+    it('calls \'cheerio.load()\'', () => {
       spyOn(cheerio, 'load')
       HtmlParser.findElement('', '')
       expect(cheerio.load).toHaveBeenCalled()
+    })
+    it('returns expected \'div\' element from cheerio', () => {
+      const expectedResult = '<div class="crow">Crow</div>'
+      const html = `<div class="sparrow">Sparrow</div>${expectedResult}<div class="raven">Raven</div>`
+      const selector = '.crow'
+      const result = HtmlParser.findElement(html, selector)
+      expect(result).toBe(expectedResult)
     })
     it('returns a string', () => {
       const result = HtmlParser.findElement('', '')
