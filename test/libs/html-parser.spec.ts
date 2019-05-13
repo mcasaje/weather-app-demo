295cf3ec29040e14d5e2901e9a47d63d6f30c9f3
Mark Casaje <mcasaje@gmail.com>
2019-05-12 21:56:51 -0700

    [1] html-parser.spec.ts: Checks a string is returned from findElement()

--- test/libs/html-parser.spec.ts
+++ test/libs/html-parser.spec.ts
@@ -6,6 +6,10 @@ describe('HtmlParser', () => {
     it('is defined', () => {
       expect(HtmlParser.findElement).toBeDefined()
     })
+    it('returns a string', () => {
+      const result = HtmlParser.findElement('')
+      expect(typeof result === 'string').toBeTruthy()
+    })
   })
 
 })
