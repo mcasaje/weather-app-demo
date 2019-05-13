5b7385f358cd84d2f2a3e5b2c3b118870dee3c17
Mark Casaje <mcasaje@gmail.com>
2019-05-12 21:25:58 -0700

    [1] request.spec.ts: Unit test to check request-promise get() was called

--- test/libs/request.spec.ts
+++ test/libs/request.spec.ts
@@ -1,9 +1,16 @@
+import * as requestPromise from 'request-promise'
 import Request from '../../src/libs/request'
 
 describe('request', () => {
   describe('get', () => {
     it('is defined', () => {
       expect(Request.get).toBeDefined()
+    })
+
+    it("calls request-promise 'get()'", () => {
+      spyOn(requestPromise, 'get')
+      Request.get('')
+      expect(requestPromise.get).toHaveBeenCalled()
     })
   })
 })
