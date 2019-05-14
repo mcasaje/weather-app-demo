871c5676884df0880c89c0eb998e499872e16fb7
Mark Casaje <mcasaje@gmail.com>
2019-05-13 22:40:55 -0700

    [1] time.spec.ts: New test to check getCurrentTime fn returns string

--- test/libs/time.spec.ts
+++ test/libs/time.spec.ts
@@ -1,9 +1,15 @@
 import Time from '../../src/libs/time'
 
 fdescribe('Time', () => {
+
   describe('getCurrentTime', () => {
     it('is defined', () => {
       expect(Time.getCurrentTime).toBeDefined()
     })
+    it('returns a string', () => {
+      const result = Time.getCurrentTime()
+      expect(typeof result === 'string').toBeTruthy()
+    })
   })
+
 })
