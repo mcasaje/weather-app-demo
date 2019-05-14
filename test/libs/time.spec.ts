7ca3686382a2eb00efd687318d2f141273e43855
Mark Casaje <mcasaje@gmail.com>
2019-05-13 23:08:39 -0700

    [1] time.spec.ts: New test to check getCurrentTime() is close to time as new Date()

--- test/libs/time.spec.ts
+++ test/libs/time.spec.ts
@@ -10,9 +10,21 @@ fdescribe('Time', () => {
       const result = Time.getCurrentTime()
       expect(typeof result === 'string').toBeTruthy()
     })
-    it("returns time in the format of 'hh:mm:ss'", () => {
+    it('returns time in the format of \'hh:mm:ss\'', () => {
       const result = Time.getCurrentTime()
       expect(result).toMatch(/\d\d:\d\d:\d\d/)
+    })
+    it('returns same time as time from \'new Date()\'', () => {
+      const result = Time.getCurrentTime()
+      const timeNow = new Date()
+      const hour = timeNow.getHours()
+      const minute = timeNow.getMinutes()
+      const seconds = timeNow.getSeconds()
+      const formattedHour = hour < 10 ? `0${hour}` : hour
+      const formattedMinute = minute < 10 ? `0${minute}` : minute
+      const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds
+      const expectedTime = `${formattedHour}:${formattedMinute}:${formattedSeconds}`
+      expect(result).toBe(expectedTime)
     })
   })
 
