8018c97bee96d88a02c243ecb24af2aabba84c39
Mark Casaje <mcasaje@gmail.com>
2019-05-13 23:53:35 -0700

    [1] weather.spec.ts: Unit test to check fetchTextFromUrl exists and unit test to check proper text is returned

--- test/weather.spec.ts
+++ test/weather.spec.ts
@@ -1,7 +1,7 @@
+import * as requestPromise from 'request-promise'
 import Weather from '../src/weather'
 
 describe('Weather', () => {
-
   describe('fetchTemperature', () => {
     it('is defined', () => {
       expect(Weather.fetchTemperature).toBeDefined()
@@ -14,16 +14,30 @@ describe('Weather', () => {
     })
   })
 
+  describe('fetchTextFromUrl', () => {
+    it('is defined', () => {
+      expect(Weather.fetchTextFromUrl).toBeDefined()
+    })
+    it('returns temperature from external endpoint', () => {
+      const url = 'http://www.fakeurl.com'
+      const htmlSelector = '#a .b'
+      const expectedText = '30'
+      const htmlResponse = `<div id="a">A<span class="b">${expectedText}</span>C</div>`
+      spyOn(requestPromise, 'get').and.returnValue(Promise.resolve(htmlResponse))
+      const result = Weather.fetchTextFromUrl(url, htmlSelector)
+      expect(result).toBe(expectedText)
+    })
+  })
+
   describe('logString', () => {
     it('is defined', () => {
       expect(Weather.logString).toBeDefined()
     })
-    it('calls \'console.log\'', () => {
+    it("calls 'console.log'", () => {
       const stringToLog = 'i-am-log'
       spyOn(console, 'log')
       Weather.logString(stringToLog)
       expect(console.log).toHaveBeenCalledWith(stringToLog)
     })
   })
-
 })
