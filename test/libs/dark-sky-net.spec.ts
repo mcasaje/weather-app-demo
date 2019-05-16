fd7ddc5e50079dca3bb218c11950a81698ac4ac2
Mark Casaje <mcasaje@gmail.com>
2019-05-15 23:06:33 -0700

    [1] dark-sky-net.spec.ts: Unit tests to check coordinates from location name or from postal code will return expected latitude and longitude

--- test/libs/dark-sky-net.spec.ts
+++ test/libs/dark-sky-net.spec.ts
@@ -1,4 +1,5 @@
-import Weather from '../../src/libs/dark-sky-net'
+import * as requestPromise from 'request-promise'
+import Weather, {default as DarkSkyNet} from '../../src/libs/dark-sky-net'
 
 describe('DarkSkyNet', () => {
   describe('readTemperature', () => {
@@ -22,6 +23,44 @@ describe('DarkSkyNet', () => {
       const weatherText = `55˚ ${expectedForecast}`
       const forecast = Weather.readForecast(weatherText)
       expect(forecast).toBe(expectedForecast)
+    })
+  })
+
+  describe('fetchGeoCoordinatesForLocationName', () => {
+    it('is defined', () => {
+      expect(DarkSkyNet.fetchGeoCoordinatesForLocationName).toBeDefined()
+    })
+    it('returns geocodes from DarkSky.Net', () => {
+      const locationName = 'Vancouver, BC'
+      const expectedLatitude = 49.2609
+      const expectedLongitude = -123.114
+      const mockGeoQueryResponse = {
+        latitude: expectedLatitude,
+        longitude: expectedLongitude,
+      }
+      spyOn(requestPromise, 'get').and.returnValue(mockGeoQueryResponse)
+      const geoLocation = DarkSkyNet.fetchGeoCoordinatesForLocationName(locationName)
+      expect(geoLocation.latitude).toBe(expectedLatitude)
+      expect(geoLocation.longitude).toBe(expectedLongitude)
+    })
+  })
+
+  describe('fetchGeoCoordinatesForPostalCode', () => {
+    it('is defined', () => {
+      expect(DarkSkyNet.fetchGeoCoordinatesForPostalCode).toBeDefined()
+    })
+    it('returns geocodes from DarkSky.Net', () => {
+      const postalCode = 'V5A 1P2'
+      const expectedLatitude = 49.2634
+      const expectedLongitude = -122.955
+      const mockGeoQueryResponse = {
+        latitude: expectedLatitude,
+        longitude: expectedLongitude,
+      }
+      spyOn(requestPromise, 'get').and.returnValue(mockGeoQueryResponse)
+      const geoLocation = DarkSkyNet.fetchGeoCoordinatesForPostalCode(postalCode)
+      expect(geoLocation.latitude).toBe(expectedLatitude)
+      expect(geoLocation.longitude).toBe(expectedLongitude)
     })
   })
 })
