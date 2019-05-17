78994ee2fc1e47cfe11eeb940f3df38d3af09bc8
Mark Casaje <mcasaje@gmail.com>
2019-05-16 19:17:41 -0700

    [1] weather.spec.ts: Unit tests for fetchWeatherForLocations return type

--- test/weather.spec.ts
+++ test/weather.spec.ts
@@ -19,6 +19,27 @@ describe('Weather', () => {
     })
   })
 
+  describe('fetchWeatherForLocations', () => {
+    it('is defined', () => {
+      expect(Weather.fetchWeatherForLocations).toBeDefined()
+    })
+    it('returns an array of object containing forecast and temperatures', () => {
+      const locations: string[] = ['Vancouver, BC']
+      const weatherData = Weather.fetchWeatherForLocations(locations)
+      expect(weatherData[0]).toBeDefined()
+      expect(weatherData[0].forecast).toBeDefined()
+      expect(weatherData[0].temperature).toBeDefined()
+      expect(weatherData[0].temperature.celsius).toBeDefined()
+      expect(weatherData[0].temperature.fahrenheit).toBeDefined()
+    })
+    it('returns an array containing weather for each location', () => {
+      const locations: string[] = ['Vancouver, BC', 'Calgary, AB', 'Toronto, ON']
+      const weatherData = Weather.fetchWeatherForLocations(locations)
+      expect(weatherData).toBeDefined()
+      expect(weatherData.length).toEqual(3)
+    })
+  })
+
   describe('formatOutput', () => {
     it('is defined', () => {
       expect(Weather.formatOutput).toBeDefined()
